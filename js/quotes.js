88008095e9cb31149a582f2e738d4423590b6b88
Glitch (glitch-hello-website) <none>
2021-07-20 21:27:33 +0000

    🏞📼 Checkpoint ./js/quotes.js:9137346/9718

--- js/quotes.js
+++ js/quotes.js
@@ -3,88 +3,114 @@
 const EMULATOR_LOOP_INTERVAL = 8;
 
 const keyToButton = {
-  "ArrowRight": "right",
-  "ArrowLeft": "left",
-  "ArrowUp": "up",
-  "ArrowDown": "down",
-  "x": "a",
-  "z": "b",
-  "Shift": "select",
-  "Enter": "start"
+  ArrowRight: "right",
+  ArrowLeft: "left",
+  ArrowUp: "up",
+  ArrowDown: "down",
+  x: "a",
+  z: "b",
+  Shift: "select",
+  Enter: "start"
 };
 
 const buttonToKeycode = {
-  "right": 0, 
-  "left": 1,
-  "up": 2,
-  "down": 3,
-  "a": 4,
-  "b": 5,
-  "select": 6,
-  "start": 7
+  right: 0,
+  left: 1,
+  up: 2,
+  down: 3,
+  a: 4,
+  b: 5,
+  select: 6,
+  start: 7
 };
 
 window.debug = function() {
-  console.log('debug:', ...arguments);
-}
+  console.log("debug:", ...arguments);
+};
 
 const fsm = new StateMachine({
   init: "idle",
   transitions: [
-    {name: "dropQuote", from: ["idle",
-                               "watching",
-                               "riffing",
-                               "playing",
-                               "recording"], to: "watching"},
-    {name: "dropGame", from: ["idle",
-                               "watching",
-                               "riffing",
-                               "playing",
-                               "recording"], to: "playing"},
-    
-    {name: "tap", from: "watching", to: "riffing"},
-    {name: "tap", from: "riffing", to: "watching"},
-    {name: "tap", from: "playing", to: "recording"},
-    {name: "tap", from: "recording", to: "compiling"},
-    
-    {name: "complete", from: "compiling", to: "playing"},
+    {
+      name: "dropQuote",
+      from: ["idle", "watching", "riffing", "playing", "recording"],
+      to: "watching"
+    },
+    {
+      name: "dropGame",
+      from: ["idle", "watching", "riffing", "playing", "recording"],
+      to: "playing"
+    },
+
+    { name: "tap", from: "watching", to: "riffing" },
+    { name: "tap", from: "riffing", to: "watching" },
+    { name: "tap", from: "playing", to: "recording" },
+    { name: "tap", from: "recording", to: "compiling" },
+
+    { name: "complete", from: "compiling", to: "playing" }
   ],
   data: {
-    button: document.getElementById('button'),
+    button: document.getElementById("button"),
     currentGame: null,
     currentQuote: null,
     currentTrace: null,
     handleJoyPadEvent: {},
-    handleExecuteIteration: {},
+    handleExecuteIteration: {
+      get: function() {
+        console.log('tick');
+        Reflect.apply(...arguments);
+      }
+    },
     runInterval: null,
     gameboy: null
   },
   methods: {
     onEnterPlaying: function() {
-      
       let canvas = document.getElementById("screen");
-      this.gameboy = GameBoyCore(canvas, this.currentGame, { drawEvents: true });
-      
-      this.gameboy.JoyPadEvent = new Proxy(
-        this.gameboy.JoyPadEvent,
-        this.handleJoyPadEvent);
-      
-      this.gameboy.executeIteration = new Proxy(
-        this.gameboy.executeIteration,
-        this.handleExecuteIteration);
-      
+      this.gameboy = GameBoyCore(canvas, this.currentGame, {
+        drawEvents: true
+      });
+
       this.gameboy.stopEmulator = 1; // required for some reason
       this.gameboy.start();
       
+      this.gameboy.JoyPadEvent = new Proxy(
+        this.gameboy.JoyPadEvent,
+        this.handleJoyPadEvent
+      );
+
+      this.gameboy.executeIteration = new Proxy(
+        this.gameboy.executeIteration,
+        this.handleExecuteIteration
+      );
+
       this.button.value = "Record new quote";
-      
+
       this.runInterval = setInterval(function() {
         fsm.gameboy.run();
       }, EMULATOR_LOOP_INTERVAL);
+    },
+
+    onEnterRecording: function() {
+      
+      this.handleExecuteIteration.get = function() {
+        console.log('iteration!');
+        Reflect.apply(...arguments);
+      }
+      
+      this.button.value = "Stop recording";
+    },
+    
+    onLeaveRecording: function() {
+      delete this.handleExecuteIteration.get;
+    },
+
+    onEnterCompiling: function() {
+      this.button.value = "Compiling...";
+      this.button.disabled = true;
     }
   }
 });
-
 
 class Quote {
   rom;
@@ -128,27 +154,28 @@ class Trace {
 - compiling,complete,playing (at *start* of recording for another take)
 
 */
-  
+
 loadGB();
 
 async function loadGB() {
   let resource = "https://bonsaiden.github.io/Tuff.gb/roms/game.gb";
   let buffer = await (await fetch(resource)).arrayBuffer();
   let rom = new Uint8Array(buffer);
-  
+
   fsm.currentGame = rom;
   fsm.dropGame();
-  
-  
+
   function handleKey(event) {
-    let down = (event.type == "keydown")
+    let down = event.type == "keydown";
     let key = event.key;
     if (key in keyToButton) {
       let keycode = buttonToKeycode[keyToButton[key]];
       fsm.gameboy.JoyPadEvent(keycode, down);
-      console.log(keycode + " " + down)
+      console.log(keycode + " " + down);
     }
   }
   document.addEventListener("keydown", handleKey, false);
   document.addEventListener("keyup", handleKey, false);
+
+  document.getElementById("button").onclick = () => fsm.tap();
 }
