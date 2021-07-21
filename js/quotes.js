61897d3cbdd01b43ed02b444e245f1fdcf0c35ea
Glitch (glitch-hello-website) <none>
2021-07-21 20:59:32 +0000

    🌕👖 Checkpoint ./archival_readme.md:9137346/2318 ./js/quotes.js:9137346/2546

--- js/quotes.js
+++ js/quotes.js
@@ -20,6 +20,26 @@ class Trace {
 const SAVESTATE_ROM = 0;
 const SAVESTATE_FRAMEBUFFER = 71;
 
+const ARCHIVAL_README_TEMPLATE = `
+This archive represents a *playable quote* of a Game Boy game.
+
+Playable quotes are delimited references to specific moments in a game along with a reference recording of how that moment of interactivity can play out.
+
+* \`rom.bin\`: A Game Boy ROM image (comparable to many \`.gb\` files) with many bytes zeroed out. While the format of this file mostly matches that used by menu emulators, this ROM image *cannot* be used to boot the game.
+
+
+* \`romMask.bin\`: This file is the same size as \`rom.bin\`, but it uses values 1 (valid) and 0 (invalid) to indicate which bytes of the ROM image are included in the quote. It should be possible to play back the recorded actions in the quote without ever reading from one of the invalid ROM addresses.
+
+* \`initialState.msgpack\`: A [MessagePack](https://github.com/msgpack/msgpack-javascript) encoded savestate for a [specific Game Boy emulator](https://github.com/rauchg/gameboy). After decoding, entry 0 of the resulting array should be replaced with a reference to the contents of the ROM image above. Additionally, entry 71 should be replaced with a reference to a 160 * 144 entry Int32Array with the decoded contents of the encoding PNG file (representing the screen visible at the start of the recorded actions).
+
+* \`actions.msgpack\`: A MessagePack-encoded array of instructions of which values to pass to \`gameboy.JoyPadEvent\` based on the number of previous calls to \`gameboy.executeIteration\`. Or maybe it is something like this. The format is currently changing, and this archive comes from a time when we hadn't worked out all of the details. Allowing the game to continue execution past the end of recorded actions (or attempting alternate actions) might result in reads to invalided ROM addresses. When this happens, it might be a good idea to return to the provided initial state.
+
+Details about this specific quote:
+DETAILS_GO_HERE
+
+By Adam Smith (adam@adamsmith.as) and Joël Franusic (joel@franusic.com) in the year 2021.
+`;
+
 
 function generateMaskedROM(rom, dependencies) {
   
@@ -87,16 +107,19 @@ async function loadQuote(buffer) {
 
 async function compileQuote(trace) {
 
+  
   let {maskedROM, mask} = generateMaskedROM(
     trace.initialState[SAVESTATE_ROM],
     trace.romDependencies);
 
-  let zip = new JSZip();
+  let originalBytes = trace.initialState[SAVESTATE_ROM].length;
+  let includedBytes = trace.initialState[SAVESTATE_ROM].map((e) => e==1).reduce((a,b)=>a+b,0);
   
   let state = trace.initialState.slice();
   state[SAVESTATE_ROM] = null; // rom+mask stored in separate zip entries
   state[SAVESTATE_FRAMEBUFFER] = null; // stored in outer PNG
 
+  let zip = new JSZip();
   zip.file("rom.bin", maskedROM);
   zip.file("romMask.bin", mask);
   zip.file("initialState.msgpack", msgpack.serialize(state));
