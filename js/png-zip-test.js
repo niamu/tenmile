94b18f243e259838b92ef925e37cf3fc8ffad163
Glitch (oo-playable-quotes) <none>
2021-07-27 19:08:19 +0000

    🍤🔔 Checkpoint ./js/png-zip-test.js:1886802/1498 ./png-zip-test.html:1886802/336

--- js/png-zip-test.js
+++ js/png-zip-test.js
@@ -1,7 +1,32 @@
 function print(line) {
-  document.getElementById("output").innerHTML += "test<br/>";  
+  document.getElementById("output").innerHTML += line + "<br/>";  
+}
+
+const zip_file = "https://cdn.glitch.com/80f5a65b-f7e3-4b40-b639-8e2c014de0ca%2Fsweet-jesus.zip";
+const png_file = "https://cdn.glitch.com/80f5a65b-f7e3-4b40-b639-8e2c014de0ca%2Fsweet-jesus-pooh.png";
+
+function concatenate(first, second) {
+  if(!first) { 
+    return second;
+  } else if (!second) {
+    return first;
+  }
+  var combined = new Uint8Array(first.byteLength + second.byteLength);
+  combined.set(new Uint8Array(first), 0);
+  combined.set(new Uint8Array(second), first.byteLength);
+  return combined.buffer;
 }
 
 (async function onPageLoad() {
-  document.getElementById("output").innerHTML += "test";
+  print("hello");
+  print("world");
+  let zip_buffer = await (await fetch(zip_file)).arrayBuffer();
+  let png_buffer = await (await fetch(png_file)).arrayBuffer();
+
+  let combined = concatenate(png_buffer, zip_buffer);
 })();
+
+/*
+<!--
+-->
+*/
