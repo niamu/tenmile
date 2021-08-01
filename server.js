d76f2dc8c6d404f57e0fb65bc3474e18070385c4
Glitch (oo-playable-quotes) <none>
2021-08-01 06:46:41 +0000

    🎸🚛 Checkpoint ./server.js:1886802/24939

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const app = express();
 
-const UNPROCESSABLE_ENTITY = 422
+const UNPROCESSABLE = 422; // HTTP 422 Unprocessable Entity
 
 const JSZip = require("jszip");
 //const zip = new JSZip();
 
 const fiftyKilobytesInBytes = 50 * 1024;
 const maximumFileSizeInBytes = fiftyKilobytesInBytes;
+const maximumByteRatioInQuote = 0.3;
 
 app.use(
   fileUpload({
@@ -30,38 +31,51 @@ app.post("/upload", async function(req, res) {
   }
 
   let zip = await JSZip().loadAsync(req.files.file.data);
-  
-  const containsRequiredFiles =
-    "rom.bin" in zip.files &&
-    "romMask.bin" in zip.files &&
-    "initialState.msgpack" in zip.files;
-  if(!containsRequiredFiles) {
-    res.status(UNPROCESSABLE_ENTITY).send('Required files are not in the playable quote');
+
+  if (
+    !(
+      "rom.bin" in zip.files &&
+      "romMask.bin" in zip.files &&
+      "initialState.msgpack" in zip.files
+    )
+  ) {
+    res.status(UNPROCESSABLE).send("Required files are missing");
   }
-  
-  let rom = await zip.file('rom.bin').async("uint8array");
-  let romMask = await zip.file('romMask.bin').async("uint8array");
-  if(rom.length != romMask.length) {
-    res.status(UNPROCESSABLE_ENTITY).send('Lengths of rom.bin and romMask.bin do not match');
+
+  let rom = await zip.file("rom.bin").async("uint8array");
+  let romMask = await zip.file("romMask.bin").async("uint8array");
+  if (rom.length != romMask.length) {
+    res.status(UNPROCESSABLE).send("rom.bin and romMask.bin length mismatch");
   }
-  
-  let contents = {}
+
+  let maskContents = {};
   romMask.map(function(byte) {
-    if(!(byte in contents)) {
-      contents[byte] = 1;
+    if (!(byte in maskContents)) {
+      maskContents[byte] = 1;
     }
-    contents[byte] += 1;
+    maskContents[byte] += 1;
   });
-  
-  console.log(containsRequiredFiles);
-  console.log(contents);
-  // HTTP 422 Invalid romMask.bin
-  console.log(Object.keys(contents).length);
-  
-  // HTTP 422 Too many valid bytes included in rom.bin
-  
+
+  if (Object.keys(maskContents).length != 2) {
+    res.status(UNPROCESSABLE).send("romMask.bin contains invalid data");
+  }
+
+  let byteRatio = maskContents["1"] / (maskContents["1"] + maskContents["0"]);
+  if (byteRatio > maximumByteRatioInQuote) {
+    res.status(UNPROCESSABLE).send("Too many valid bytes included in rom.bin");
+  }
+
+  let romContents = {};
+  rom.map(function(byte) {
+    if (!(byte in romContents)) {
+      romContents[byte] = 1;
+    }
+    romContents[byte] += 1;
+  });
+
+  console.log(romContents);
   // HTTP 422 Semantic mismatch between romMask.bin and rom.bin
-  
+
   res.send("testing");
 });
 
