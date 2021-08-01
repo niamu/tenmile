7f0a853c0eed0460b43449141a1f3e031b9f9410
Glitch (oo-playable-quotes) <none>
2021-08-01 06:23:43 +0000

    ❣️🌘 Checkpoint ./server.js:1886802/3959

--- server.js
+++ server.js
@@ -1,43 +1,63 @@
-const express = require('express')
-const fileUpload = require('express-fileupload');
-const app = express()
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const app = express();
 
-const JSZip = require('jszip');
+const JSZip = require("jszip");
 //const zip = new JSZip();
 
 const fiftyKilobytesInBytes = 50 * 1024;
 
-app.use(fileUpload({
-  abortOnLimit: true,
-  limits: {
-    fileSize: fiftyKilobytesInBytes,
-    fields: 1,
-    files: 1
-  }
-}));
+app.use(
+  fileUpload({
+    abortOnLimit: true,
+    limits: {
+      fileSize: fiftyKilobytesInBytes,
+      fields: 1,
+      files: 1
+    }
+  })
+);
 
-app.post('/upload', async function (req, res) {
-  if(!req.files) {
+app.post("/upload", async function(req, res) {
+  if (!req.files) {
     res.send({
       status: false,
       message: "No files sent"
     });
   }
-  
-  let zipContents = {}
+
   let zip = await JSZip().loadAsync(req.files.file.data);
-  for (let filename of Object.keys(zip.files)) {
-    zipContents[filename] = true;
-    console.log(filename);
-  }
+  const containsRequiredFiles =
+    "rom.bin" in zip.files &&
+    "romMask.bin" in zip.files &&
+    "initialState.msgpack" in zip.files;
+
+  // error if required files are not in zip
   
-  const containsRequiredFiles = (
-    'rom.bin' in zip.files &&
-    'romMask.bin' in zip.files &&
-    'initialState.msgpack' in zip.files
-  );
+  let rom = await zip.file('rom.bin').async("uint8array");
+  let romMask = await zip.file('romMask.bin').async("uint8array");
+
+  console.log(rom.length);
+  console.log(romMask.length);
+  // error if lengths of rom and romMask do not match
+  
+  
+  let contents = {}
+  romMask.map(function(byte) {
+    if(!(byte in contents)) {
+      contents[byte] = 1;
+    }
+    contents[byte] += 1;
+  });
   
   console.log(containsRequiredFiles);
+  console.log(contents);
+  // error if rom mask has more than 2 keys
+  console.log(Object.keys(contents).length);
+  
+  // error if percentage of ROM mask is about a threshold
+  
+  //error if number of 0s in ROM Mask is < number of 0s in ROM
   
   res.send("testing");
 });
